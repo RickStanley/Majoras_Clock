e830efcb300d2d65b70370455c74cc8ec64c7a35
Rick Stanley <rick-stanley@outlook.com>
2020-08-11 19:59:45 -0400

    fix: unregister global shortcut

--- src/app.js
+++ src/app.js
@@ -186,6 +186,7 @@ const createWindow = (maximize = true) => {
 
   // Emitted when the window is closed.
   mainWindow.on("closed", () => {
+    unregisterShorcuts();
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
