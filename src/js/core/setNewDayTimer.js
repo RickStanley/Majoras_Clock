28d59ef232c0395a3fd850d63416d866d1c913cc
Rick Stanley <rick-stanley@outlook.com>
2020-08-12 00:19:26 -0400

    feat: bells for midnight day transitions

--- src/js/core/setNewDayTimer.js
+++ src/js/core/setNewDayTimer.js
@@ -2,16 +2,31 @@
 const { ipcRenderer } = require("electron");
 //#endregion
 
+import { getAudioDuration } from "../utils/misc.js";
+
 /**
  * Function to new day start.
  */
-function setNewDayTimer() {
+async function setNewDayTimer() {
   if (globalThis.newdaytimer)
     clearTimeout(newdaytimer);
+
+  const BELLS = new Audio("../assets/sounds/MM_ClockTower_Bell.wav");
+  const BELLS_DURATION = await getAudioDuration(BELLS);
+  const END_MILLISECONDS = (~~BELLS_DURATION) * 1000;
+
   const NOW = new Date();
   const TOMORROW = new Date(NOW.getFullYear(), NOW.getMonth(), NOW.getDate() + 1);
-  const DISTANCE = TOMORROW - NOW;
-  globalThis.newdaytimer = setTimeout(() => ipcRenderer.send("newDay"), DISTANCE);
+  // Subtract the duration of the bells' audio.
+  const DISTANCE = TOMORROW - NOW - END_MILLISECONDS;
+
+  BELLS.addEventListener("ended", () => {
+    ipcRenderer.send("newDay");
+  });
+
+  globalThis.newdaytimer = setTimeout(() => {
+    BELLS.play();
+  }, DISTANCE);
 }
 
 export default setNewDayTimer;
