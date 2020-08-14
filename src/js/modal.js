8884cc0b8a63c35ebed56d576d85a8e3657a501f
Rick Stanley <rick-stanley@outlook.com>
2020-08-13 21:08:18 -0400

    refactor: separate form fields builders

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,60 +6,9 @@ const { ipcRenderer } = require("electron");
 import { ready, attach } from "./utils/misc.js";
 import InputRange from "./components/inputRange.js";
 import TerminaClock from "./components/terminaClock.js";
+import setupGroupSettings from "./utils/formFields.js";
 
 const SETTINGS_TEMPLATE = document.createElement("template");
-
-const createRadios = (values, { name = "" } = {}) => values.map(({ value, label }) => `
-<label class="radio">
-  <input type="radio" name="${name}" value="${value}">
-  <i></i>
-  <span>${label}</span>
-</label>
-`);
-
-const createToggle = ({ label = "", name = "", checked = false } = {}) => `
-<label class="toggle">
-  <input name="${name}" type="checkbox" ${checked ? "checked" : ""} >
-  <i></i>
-  <span>${label}</span>
-</label>
-`;
-
-const createRange = ({ label = "", value = "1", name = "", min = "1", max = "4" } = {}) => `
-${label ? `<label>${label}</label>` : ""}
-<div class="range" is="input-range">
-  <input type="range" name="${name}" value="${value}" min="${min}" max="${max}">
-  <output>${value}</output>
-</div>
-`;
-
-/**
- * @param {{label: string, value: string}[]} values 
- */
-const createSelect = (values, { name = "" } = {}) => `
-<div class="select">
-  <select name="${name}">
-  ${values.map(({ label, value }) => `<option value="${value}">${label}</option>`).join("")}
-  </select>
-</div>
-`;
-
-/**
- * 
- * @param {{legend?: string, type: string, name?: string, fields: { label: string, name: string, value?: any}[]}} GROUP 
- */
-const setupGroupSettings = GROUP => {
-  switch (GROUP.type) {
-    case "toggle":
-      return GROUP.fields.map(createToggle).join("");
-    case "range":
-      return GROUP.fields.map(createRange).join("");
-    case "radio":
-      return createRadios(GROUP.fields, { name: GROUP.name }).join("");
-    case "select":
-      return createSelect(GROUP.fields, { name: GROUP.name });
-  }
-};
 
 SETTINGS_TEMPLATE.innerHTML = `
   <header class="modal__header">
